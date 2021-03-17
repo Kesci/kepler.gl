184e6e75285fb2916b4aa1774560303d34442904
Ilya Boyandin <ilyabo@gmail.com>
2021-03-17 21:04:59 +0100

    [Feat] Add pin map legend to map control

--- src/components/map/map-legend-panel.js
+++ src/components/map/map-legend-panel.js
@@ -18,26 +18,29 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-import React, {useCallback} from 'react';
-import classnames from 'classnames';
+import React, {useState} from 'react';
+import styled from 'styled-components';
+
 import {Legend} from 'components/common/icons';
 import {FormattedMessage} from 'localization';
-import {MapControlButton, Tooltip} from 'components/common/styled-components';
+import {MapControlButton} from 'components/common/styled-components';
 import MapControlTooltipFactory from './map-control-tooltip';
 import MapControlPanelFactory from './map-control-panel';
 import MapLegendFactory from './map-legend';
-
-const MapLegendTooltip = ({id, message}) => (
-  <Tooltip id={id} place="left" effect="solid">
-    <span>
-      <FormattedMessage id={message} />
-    </span>
-  </Tooltip>
-);
+import Tippy from '@tippyjs/react/headless';
+import TippyTooltip from 'components/common/tippy-tooltip';
+import {createPortal} from 'react-dom';
+import {DIMENSIONS} from 'constants/default-settings';
 
 MapLegendPanelFactory.deps = [MapControlTooltipFactory, MapControlPanelFactory, MapLegendFactory];
 
-function MapLegendPanelFactory(MapControlTooltip, MapControlPanel, MapLegend) {
+const PinToBottom = styled.div`
+  position: absolute;
+  bottom: ${DIMENSIONS.mapControl.mapLegend.pinned.bottom}px;
+  right: ${DIMENSIONS.mapControl.mapLegend.pinned.right}px;
+`;
+
+function MapLegendPanelFactory(MapControlPanel, MapLegend) {
   const defaultActionIcons = {
     legend: Legend
   };
@@ -50,46 +53,94 @@ function MapLegendPanelFactory(MapControlTooltip, MapControlPanel, MapLegend) {
     onToggleMapControl,
     isExport,
     logoComponent,
-    actionIcons = defaultActionIcons
+    actionIcons = defaultActionIcons,
+    mapHeight
   }) => {
     const mapLegend = mapControls?.mapLegend || {};
-    const {active: isActive, disableClose} = mapLegend || {};
-    const onToggleMenuPanel = useCallback(() => onToggleMapControl('mapLegend'), [
-      onToggleMapControl
-    ]);
+    const {active: isPinned} = mapLegend || {};
 
-    const onClick = useCallback(
-      e => {
-        e.preventDefault();
-        onToggleMenuPanel();
-      },
-      [onToggleMenuPanel]
-    );
+    const onClick = () => {
+      if (mapControls?.mapDraw?.active) {
+        onToggleMapControl('mapDraw');
+      }
+    };
+    const [tippyInstance, setTippyInstance] = useState(null);
+    const onCloseClick = e => {
+      e.preventDefault();
+      onToggleMapControl('mapLegend');
+    };
+    const onPinClick = e => {
+      e.preventDefault();
+      if (tippyInstance) {
+        // @ts-ignore
+        tippyInstance.hide();
+      }
+      onToggleMapControl('mapLegend');
+    };
 
     if (!mapLegend.show) {
       return null;
     }
-    return !isActive ? (
-      (<MapControlButton
-        data-tip
-        data-for="show-legend"
-        className={classnames('map-control-button', 'show-legend', {isActive})}
-        onClick={onClick}
-      >
-        <actionIcons.legend height="22px" />
-        <MapLegendTooltip id="show-legend" message={'tooltip.showLegend'} />
-      </MapControlButton>)
-    ) : (
-      (<MapControlPanel
+    const mapControlPanel = (
+      <MapControlPanel
         scale={scale}
         header={'header.layerLegend'}
-        onClick={onToggleMenuPanel}
+        isPinned={true}
+        {...(isPinned
+          ? {
+              onClick: onCloseClick,
+              pinnable: false,
+              disableClose: false
+            }
+          : {
+              onPinClick,
+              pinnable: true,
+              disableClose: true
+            })}
         isExport={isExport}
-        disableClose={disableClose}
         logoComponent={logoComponent}
       >
-        <MapLegend layers={layers} />
-      </MapControlPanel>)
+        <MapLegend layers={layers} mapHeight={mapHeight} />
+      </MapControlPanel>
+    );
+
+    if (isPinned) {
+      // Pinned panel is not supported in export mode
+      if (isExport) {
+        return mapControlPanel;
+      }
+      const pinnedPanel = <PinToBottom>{mapControlPanel}</PinToBottom>;
+      return createPortal(pinnedPanel, document.body);
+    }
+
+    return (
+      // The outer div is to prevent an accessibility warning from Tippy
+      <div>
+        <Tippy
+          interactive={true}
+          trigger="click"
+          placement="left-start"
+          // @ts-ignore
+          onCreate={setTippyInstance}
+          render={attrs => <div {...attrs}>{mapControlPanel}</div>}
+          appendTo={document.body}
+        >
+          <div>
+            <TippyTooltip
+              placement="left"
+              render={() => (
+                <div id="show-legend">
+                  <FormattedMessage id="tooltip.showLegend" />
+                </div>
+              )}
+            >
+              <MapControlButton className="map-control-button show-legend" onClick={onClick}>
+                <actionIcons.legend height="22px" />
+              </MapControlButton>
+            </TippyTooltip>
+          </div>
+        </Tippy>
+      </div>
     );
   };
 
